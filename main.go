ae1a8c0401246cbda3e5cc0e1823ae8d1dbe51e6
Matthew Brandeburg <33352235+mbrandeburg@users.noreply.github.com>
2019-01-07 15:10:22 -0500

    First incorporation of main.go
    Will need to pull in deck files, created by gophercises

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/gophercises/deck"	
+	"fmt"
+)
+
+func main() {
+	cards := deck.New(deck.Shuffle)
+	var card deck.Card
+	for i := 0; i < 10; i++ { //let's iterate over (aka deal out) 10 cards to start with
+		card, cards = cards[0], cards[1:] //pulling out the first card, leaving a slice leftover with all the rest of cards till the i++ iterator runs out
+		fmt.Println(card)
+	}
+}
