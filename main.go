d0c7a8ff1b0814b8f119f289493d4cc073507c36
Matthew Brandeburg <matthewbrandeburg@Matthews-MacBook-Pro.local>
2019-01-08 16:04:48 -0500

    rocking with two rounds

--- main.go
+++ main.go
@@ -23,13 +23,11 @@ func draw(cards []deck.Card) (deck.Card, []deck.Card){
 
 
 func main() {
-	cards := deck.New(deck.Shuffle)
-	var card deck.Card
 	fmt.Println("\n\n\nWELCOME TO\n")
 	time.Sleep(1 * time.Second)
 	fmt.Println("SMOKE\n OR\n FIRE\n") //FIRE fire fire fire (whisper) fire
 	time.Sleep(1 * time.Second)
-	fmt.Println("I have two questions for you..")
+	fmt.Println("I have three questions for you..")
 	time.Sleep(1 * time.Second)
 
 	var playerCount int
@@ -49,13 +47,27 @@ func main() {
 			})
 		}
 
+	var deckCount int
+	for {
+		fmt.Println("Second, how many decks are we playing with? (1-20)")
+		fmt.Scanf("%d\n", &deckCount)
+		if deckCount >= 1 && deckCount <= 20 {
+			break
+		}
+		fmt.Println("Please enter a number in the valid range.")
+	}
+	cards := deck.New(deck.Deck(deckCount), deck.Shuffle)
+	var card deck.Card
+
+
 	// for currPlayer := 0; ; currPlayer = (currPlayer + 1) % len(players){
 	time.Sleep(1 * time.Second)
-	fmt.Println("Second, for Player1, my question is...")
+	fmt.Println("Lastly, for Player1, my question is...")
+	var input string
+
 	for _, p := range players {
 		card, cards = draw(cards)
 		p.Hand = append(p.Hand,card)
-		var input string
 		time.Sleep(1 * time.Second)
 		fmt.Printf("Player %d: (S)moke or (F)ire?\n", p.Number)
 		fmt.Scanf("%s\n", &input)
@@ -74,39 +86,40 @@ func main() {
 				fmt.Printf("Player %d drew the %s and has to drink for one second.\n", p.Number, p.Hand[0])
 			}
 		}
-	}
+	} 
+	time.Sleep(1 * time.Second) // pause afer the player's round
 
-	// for round := 0; round < 3; round ++ {
-	// 	for _, p := range players {
-	// 		card, cards = draw(cards)
-	// 		p.Hand = append(p.Hand,card)
-			
-	// 		time.Sleep(1 * time.Second)
-	// 		fmt.Printf("Player %d: (H)igher or (L)ower?\n", p.Number)
-	// 		fmt.Scanf("%s\n", &input)
+	for round := 0; round < 3; round ++ {
+		for _, p := range players {
+			card, cards = draw(cards)
+			p.Hand = append(p.Hand,card)
+			time.Sleep(1 * time.Second)
+			fmt.Printf("Player %d: (H)igher or (L)ower?\n", p.Number)
+			fmt.Scanf("%s\n", &input)
 
-	// 		prev := p.Hand[round]
-	// 		if card.Rank == prev.Rank {
-	// 			fmt.Printf("Player%d had a tie with %s and is safe!\n", p.Number, p.Hand[round])
-	// 			continue
-	// 		}
+			prev := p.Hand[round]
+			if card.Rank == prev.Rank {
+				fmt.Printf("Player%d had a tie with %s this round and %s last round, and is safe!\n", p.Number, p.Hand[round], prev)
+				continue
+			}
 
-	// 		if strings.ToLower(input) == "h" {
-	// 			if card.Rank > prev.Rank {
-	// 			fmt.Printf("Player%d had a higher card with %s than his or her previous card of %s and is safe!\n", p.Number, p.Hand[round], prev)
-	// 				} else {
-	// 				fmt.Printf("Player%d had a lower card with %s than his or her previous card of %s and has to drink for one second.\n", p.Number, p.Hand[round], prev)
-	// 			} else { // they chose lower
-	// 				if card.Rank < prev.Rank {
-	// 					fmt.Printf("Player%d had a lower card with %s than his or her previous card of %s and is safe!\n", p.Number, p.Hand[round], prev)
-	// 				} else {
-	// 					fmt.Printf("Player%d had a higher card with %s than his or her previous card of %s and has to drink for one second.\n", p.Number, p.Hand[round], prev)
-	// 				}
-	// 			}
-	// 		}
-	// 	}
-	// 	// next round in the loop! (for _, p := range players ...)
-	// }		
+			if strings.ToLower(input) == "h" {
+				if card.Rank > prev.Rank {
+				fmt.Printf("Player%d had a higher card with %s than his or her previous card of %s and is safe!\n", p.Number, p.Hand[round], prev)
+					} else {
+					fmt.Printf("Player%d had a lower card with %s than his or her previous card of %s and has to drink for one second.\n", p.Number, p.Hand[round], prev)
+				} } else { // they chose lower
+					if card.Rank < prev.Rank {
+						fmt.Printf("Player%d had a lower card with %s than his or her previous card of %s and is safe!\n", p.Number, p.Hand[round], prev)
+					} else {
+						fmt.Printf("Player%d had a higher card with %s than his or her previous card of %s and has to drink for one second.\n", p.Number, p.Hand[round], prev)
+					}
+				}
+				time.Sleep(1 * time.Second) // pause afer the player's round
+			} 
+
+		// next round in the loop! (for _, p := range players ...)
+	}		
 
 
 	// Now for everything else I guess...
