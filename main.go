c17b860217486826b74fbbb54e41c5bf85ce06c4
Matthew Brandeburg <matthewbrandeburg@Matthews-MacBook-Pro.local>
2019-01-08 13:36:53 -0500

    we have liftoff

--- main.go
+++ main.go
@@ -13,6 +13,11 @@ func draw(cards []deck.Card) (deck.Card, []deck.Card){
 	return cards[0], cards[1:]	
 }
 
+// func score(cards []deck.Card) int {
+// 	score := cards.Rank
+// 	return score
+// }
+
 func main() {
 	cards := deck.New(deck.Shuffle)
 	var card deck.Card
@@ -21,6 +26,12 @@ func main() {
 	fmt.Println("I have two questions for you..")
 
 	fmt.Println("How many players are there?")
+	/* 
+			FILL THIS IN LATER
+			need a way to make multiple players not just 1
+
+	*/
+
 	var player1 []deck.Card
 
 	fmt.Println("...and...for player1...")
@@ -31,25 +42,31 @@ func main() {
 		switch input {
 		case "s":
 			card, cards = draw(cards)
+			// score := int(card.Rank)
+			// fmt.Println(score)
 			player1 = append(player1, card)
 			fmt.Println(player1)
+			fmt.Printf("Player1 drew the %s, so drink for %v seconds!\n", player1[0], int(card.Rank)) //.Score()
 		case "f":
 			card, cards = draw(cards)
 			player1 = append(player1, card)
-			fmt.Println(player1)
+			fmt.Printf("Player1 drew the %s, so drink for %v seconds!\n", player1[0], int(card.Rank)) //.Score()
 		}
-	
-
-	for i := 0; i < 2; i++ { // not <=52 b/c there's a 0th card
-		card, cards = draw(cards)
-		fmt.Println(card)
-		// fmt.Println(i)
-	}
-	// fmt.Println(len(cards))
 }
 
 
-///////
+
+
+/////// THE LEFTOVERS
+
+	// for i := 0; i < 2; i++ { // not <=52 b/c there's a 0th card
+	// 	card, cards = draw(cards)
+	// 	fmt.Println(card)
+	// 	// fmt.Println(i)
+	// }
+	// // fmt.Println(len(cards))
+
+
 // func main() {
 // 	cards := deck.New(deck.Shuffle)
 // 	var card deck.Card
